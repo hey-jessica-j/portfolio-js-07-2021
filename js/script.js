b2247a4114356f68aa6fceac73a23b0d9518e06c
Jessica Jennings <75150881+hey-jessica-j@users.noreply.github.com>
2021-07-15 15:42:56 -0400

    added about me and animations

--- js/script.js
+++ js/script.js
@@ -27,10 +27,11 @@ let personalInfo = {
 
 //Main Document Data
 let aboutData = {
-  color: "#FF3366",
-  title: "About",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
-  image: ""
+  description: ["Math Nerd turned Web developer and Security Specialist at your service!",
+                "Shortly after completing my bachelors in mathematics, I asked myself, ok what’s next? I found myself exploring the world of programming. I dabbled in the world of web development and built a few beginner websites and applications. Somehow, I ended up on a “capture the flag” website and the term ethical Hacker became ingrained in my head. Wanting to learn more I decided to go for my masters in the field.",
+              "After earning my masters in Cybersecurity Technology I again asked what’s next? I ended up back in programming, so I enrolled in a bootcamp. I learned how to make (better) web applications, both front-end and some back-end and design them. But I was still curious about the security aspect of the web application. I decided to pursue web applications and their security as my concentration in the field and have deemed myself a web security app guru."
+              ],
+  image: "img/headshot.png"
 };
 
 let schoolData = [
@@ -41,7 +42,7 @@ let schoolData = [
 
 let  workData = [
   {title:"Morgan Stanley", date:"07/2017 - Present",
-  description: ["Started a <span>CTF (Capture the Flag) beginners blog</span> via the Baltimore Women in Technology group with other CTF enthusiasts.", "Reviewed client verification documents quickly while maintaining an average of 99% quality.", "Conduct reports for managers to audit accounts and assign validation points. <span>Worked with branches</span> to understand their needs and submit those needs to the correct parties to get solutions."
+  description: ["Started a <span>CTF (Capture the Flag) beginners blog</span> via the Baltimore Women in Technology group with other CTF enthusiasts.", "Reviewed client verification documents quickly while maintaining an average of 99% quality.", "Conduct reports for managers to audit accounts and assign validation points.", "<span>Worked with branches</span> to understand their needs and submit those needs to the correct parties to get solutions."
     ]}
 ];
 
@@ -190,9 +191,33 @@ sentenceCarousel(carousel, changingText);
 
 
 // -------------------------------
-//
+//             About Section
 // -------------------------------
 
+//
+//
+
+function addAboutInfo(aboutData, element) {
+  let p = "";
+  let paragraph = aboutData.description;
+  function addPara(value) {p = p + "<p>" + value + "</p>";};
+  paragraph.forEach(addPara);
+  let about = document.createElement("div");
+  about.innerHTML =
+    `
+    <div class="about-content hundred center">
+      <div class= "about-picture-section right">
+        <img src= "${aboutData.image}" />
+      </div>
+      <div class="about-text three-fourth left">
+        ${p}
+      </div>
+    </div>
+
+    `
+    element.append(about);
+};
+addAboutInfo(aboutData, aboutSection);
 
 // -------------------------------
 //             Resume Section
@@ -212,10 +237,10 @@ function addSchoolResumeInfo(schoolData, element) {
     article.innerHTML =
     `
     <div class="school-article hundred">
-      <div class="one-half">
+      <div class="school-data one-half right">
         <p>${schoolData[i].course}</p>
       </div>
-      <div class="one-half center">
+      <div class="school-data one-half left">
         <p> ${schoolData[i].date}</p>
         <p>${schoolData[i].school} </p>
       </div>
